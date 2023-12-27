97a7904397a4184c0e210d9a3bcb7758d6444f63
MateoGiraz <mateogiraz27@gmail.com>
2023-12-27 17:36:12 -0300

    add env database_url var

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from service.price_service import get_prices
 from repository.connection import connect
@@ -6,6 +7,7 @@ app = FastAPI()
 
 @app.get("/price")
 async def get_card(name: str = "", edition: str = "", foil: bool = False):
-    session = connect("postgresql://postgres:secret@localhost:5432/prices")
+    postgresURL = os.environ['DATABASE_URL']
+    session = connect(postgresURL)
     prices = get_prices(name, edition, foil, session)
     return list(map(lambda price: price.to_dict(), prices))
